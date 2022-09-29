a09329ad9aeef988ae2548ab2f3dafe23fefb8c6
James Robertson <jwrober@gmail.com>
2022-09-29 17:01:12 -0500

    Add new sphinx_fc21_manuals sphinx extension

---
+++ sphinx_fc21_manuals/conf.py
@@ -0,0 +1,6 @@
+# conf.py
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2022 James Robertson <jwrober@gmail.com>
+
+
---
+++ sphinx_fc21_manuals/sphinx_fc21_manuals.py
@@ -0,0 +1,16 @@
+# sphinx_fc21_manuals.py
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2022 James Robertson <jwrober@gmail.com>
+
+# Version string
+__version__ = '0.1.0'
+
+#import logging
+#import os
+#import re
+#from warnings import warn
+
+#from jinja2 import Environment, PackageLoader, select_autoescape
+#from markdown import markdown
+
