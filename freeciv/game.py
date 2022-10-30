6c01b6dcfb4d83c8c513b5339ec5bd178e8870f3
Louis Moureaux <m_louis30@yahoo.com>
2022-10-30 20:48:06 +0100

    Add games properties to support rulesets down to lt30

--- freeciv/game.py
+++ freeciv/game.py
@@ -73,24 +73,30 @@ class CivStyleData:
     upgrade_veteran_loss: int
     autoupgrade_veteran_loss: int
     pillage_select: bool
-    tech_steal_allow_holes: bool
-    tech_trade_allow_holes: bool
-    tech_trade_loss_allow_holes: bool
-    tech_parasite_allow_holes: bool
-    tech_loss_allow_holes: bool
-    civil_war_enabled: bool
-    civil_war_bonus_celebrating: int
-    civil_war_bonus_unhappy: int
-    gameloss_style: str
-    paradrop_to_transport: bool
     gold_upkeep_style: str
-    output_granularity: int
+    paradrop_to_transport: bool = True  # FIXME default?
+    gameloss_style: str = ""  # FIXME default?
+    civil_war_enabled: bool = True  # FIXME default?
+    tech_steal_allow_holes: bool = True  # FIXME default?
+    tech_trade_allow_holes: bool = True  # FIXME default?
+    tech_trade_loss_allow_holes: bool = True  # FIXME default?
+    tech_parasite_allow_holes: bool = True  # FIXME default?
+    tech_loss_allow_holes: bool = True  # FIXME default?
+    output_granularity: int = 1  # FIXME default?
+    civil_war_bonus_celebrating: int = 0  # FIXME default?
+    civil_war_bonus_unhappy: int = 0  # FIXME default?
     initial_diplomatic_state: str = "War"
     base_pollution: int = 0  # FIXME default?
     happy_cost: int = 0  # FIXME default?
     food_cost: int = 0  # FIXME default?
     base_bribe_cost: int = 0  # FIXME default?
     ransom_gold: int = 0  # FIXME default?
+    # From lt30
+    nuke_contamination: str = "Fallout"
+    base_tech_cost: int = 0
+    min_dist_bw_cities: int = 0
+    tech_cost_style: int = 0
+    tech_leakage: int = 0
 
 
 @section("illness")
@@ -119,17 +125,24 @@ class InciteCostData:
 @dataclass
 class CombatRulesData:
     tired_attack: bool
-    only_killing_makes_veteran: bool
-    nuke_pop_loss_pct: int
-    nuke_defender_survival_chance_pct: int
+    only_killing_makes_veteran: bool = False  # FIXME 3.0?
+    nuke_pop_loss_pct: int = 50
+    nuke_defender_survival_chance_pct: int = 0
+    # lt30
+    killstack: bool = True
+    # LT53
+    incite_gold_capt_chance: int = 0
+    incite_gold_loss_chance: int = 0
+    timeoutmask: int = 0
 
 
 @section("auto_attack")
 @typechecked
 @dataclass
 class AutoAttackData:
-    attack_actions: list[str]
+    attack_actions: list[str] = field(default_factory=list)  # FIXME 3.0?
     if_attacker: list[Requirement] = field(default_factory=list)
+    will_never: list[str] = field(default_factory=list)  # FIXME ???
 
 
 ActionRange = Union[int, Literal["unlimited"]]
@@ -139,18 +152,18 @@ ActionRange = Union[int, Literal["unlimited"]]
 @typechecked
 @dataclass
 class ActionsData:
-    force_trade_route: bool
-    force_capture_units: bool
-    force_bombard: bool
-    force_explode_nuclear: bool
-    help_wonder_max_range: ActionRange
-    recycle_unit_max_range: ActionRange
-    bombard_max_range: ActionRange
-    bombard_2_max_range: ActionRange
-    bombard_3_max_range: ActionRange
-    explode_nuclear_max_range: ActionRange
-    nuke_city_max_range: ActionRange
-    nuke_units_max_range: ActionRange
+    force_trade_route: bool = True  # FIXME before 3.0?
+    force_bombard: bool = True  # FIXME before 3.0?
+    force_capture_units: bool = True  # FIXME before 3.0?
+    force_explode_nuclear: bool = True  # FIXME before 3.0?
+    help_wonder_max_range: ActionRange = 1  # FIXME before 3.0?
+    recycle_unit_max_range: ActionRange = 1  # FIXME before 3.0?
+    bombard_max_range: ActionRange = 1  # FIXME before 3.0?
+    bombard_2_max_range: ActionRange = 1  # FIXME before 3.0?
+    bombard_3_max_range: ActionRange = 1  # FIXME before 3.0?
+    explode_nuclear_max_range: ActionRange = 1  # FIXME before 3.0?
+    nuke_city_max_range: ActionRange = 1  # FIXME before 3.0?
+    nuke_units_max_range: ActionRange = 1  # FIXME before 3.0?
     ui_name_bribe_unit: str = ""  # FIXME default?
     ui_name_sabotage_city: str = ""  # FIXME default?
     ui_name_incite_city: str = ""  # FIXME default?
@@ -187,6 +200,12 @@ class ActionsData:
     ui_name_transport_disembark: str = ""  # FIXME default?
     ui_name_transport_embark: str = ""  # FIXME default?
     ui_name_poison_city_escape: str = ""  # FIXME default?
+    ui_name_suitcase_nuke: str = ""  # FIXME default?
+    ui_name_heal_unit: str = ""  # FIXME default?
+    ui_name_targeted_sabotage_city: str = ""  # FIXME default?
+    ui_name_poison_city: str = ""  # FIXME default?
+    ui_name_sabotage_unit: str = ""  # FIXME default?
+    ui_name_targeted_steal_tech: str = ""  # FIXME default?
     ui_name_suitcase_nuke_escape: str = ""  # FIXME default?
     ui_name_sabotage_unit_escape: str = ""  # FIXME default?
     ui_name_sabotage_city_escape: str = ""  # FIXME default?
@@ -227,18 +246,18 @@ class ActionsData:
 class BordersData:
     radius_sq_city: int
     size_effect: int
-    radius_sq_city_permanent: int
+    radius_sq_city_permanent: int = 0  # FIXME 2.6?
 
 
 @section("research")
 @typechecked
 @dataclass
 class ResearchData:
-    tech_cost_style: str
-    base_tech_cost: int
-    tech_leakage: str
     tech_upkeep_style: str
-    free_tech_method: str
+    free_tech_method: str = ""  # FIXME default?
+    tech_leakage: str = ""  # FIXME default?
+    base_tech_cost: int = 0  # FIXME default?
+    tech_cost_style: str = ""  # FIXME default?
     tech_upkeep_divider: int = 0  # FIXME default?
 
 
@@ -248,8 +267,8 @@ class ResearchData:
 class CultureData:
     victory_min_points: int
     victory_lead_pct: int
-    history_interest_pml: int
     migration_pml: int
+    history_interest_pml: int = 0  # FIXME 3.0?
 
 
 @section("calendar")
@@ -257,9 +276,9 @@ class CultureData:
 @dataclass
 class CalendarData:
     skip_year_0: bool
-    fragments: int
     positive_label: str
     negative_label: str
+    fragments: int = 3  # FIXME default?
     fragment_name0: str = None
     fragment_name1: str = None
     start_year: int = 0  # FIXME default?
